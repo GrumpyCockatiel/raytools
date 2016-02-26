efff43e725764415d683098e261d33895a7986bf
GrumpyCockatiel <tguillory@gmail.com>
2016-02-26 15:41:36 -0600

    Test index and data
    added a data file and test page

---
+++ data.js
@@ -0,0 +1,161 @@
+﻿var myData = [{
+	"id": "d5a1b941-8bb3-4cc5-b874-87501e3fff78",
+	"gender": "Male",
+	"firstName": "Steven",
+	"lastName": "Webb",
+	"email": "swebb0@symantec.com",
+	"title": "Marketing Manager",
+	"city": "Opuzen"
+}, {
+	"id": "9ace4bad-d454-4080-92ba-673c8ef87b18",
+	"gender": "Male",
+	"firstName": "Ronald",
+	"lastName": "Perez",
+	"email": "rperez1@simplemachines.org",
+	"title": "Electrical Engineer",
+	"city": "Mantampay"
+}, {
+	"id": "18a796ef-fdc1-4d0b-9548-edacab2cc471",
+	"gender": "Female",
+	"firstName": "Annie",
+	"lastName": "Chapman",
+	"email": "achapman2@geocities.com",
+	"title": "Sales Representative",
+	"city": "Liuhuang"
+}, {
+	"id": "7e47699e-d3cd-4d4a-834c-dbc787f2f0f8",
+	"gender": "Male",
+	"firstName": "Samuel",
+	"lastName": "Jenkins",
+	"email": "sjenkins3@npr.org",
+	"title": "Professor",
+	"city": "La Unión"
+}, {
+	"id": "3317ff0e-56ac-442b-9505-d7ce1bb5f07d",
+	"gender": "Male",
+	"firstName": "Benjamin",
+	"lastName": "Collins",
+	"email": "bcollins4@weebly.com",
+	"title": "Help Desk Operator",
+	"city": "Magisterial"
+}, {
+	"id": "cae404ff-1359-4cb5-a579-e11bae65bf1a",
+	"gender": "Male",
+	"firstName": "Matthew",
+	"lastName": "Hall",
+	"email": "mhall5@symantec.com",
+	"title": "Executive Secretary",
+	"city": "Daru Rilwan"
+}, {
+	"id": "1c768834-5014-4e30-9492-7183de32844c",
+	"gender": "Male",
+	"firstName": "John",
+	"lastName": "Lawrence",
+	"email": "jlawrence6@redcross.org",
+	"title": "Analyst Programmer",
+	"city": "Gadingrejo"
+}, {
+	"id": "d9bc0832-6b84-4b1a-b240-be375ca2da4c",
+	"gender": "Female",
+	"firstName": "Lillian",
+	"lastName": "Stone",
+	"email": "lstone7@trellian.com",
+	"title": "Sales Representative",
+	"city": "Birmingham"
+}, {
+	"id": "6394fab5-1b66-4a40-a83c-456dd5d2de2c",
+	"gender": "Female",
+	"firstName": "Ruth",
+	"lastName": "Myers",
+	"email": "rmyers8@fc2.com",
+	"title": "Electrical Engineer",
+	"city": "Binalbagan"
+}, {
+	"id": "d2708052-aad6-441f-867a-f4aef9d5c08b",
+	"gender": "Male",
+	"firstName": "Daniel",
+	"lastName": "West",
+	"email": "dwest9@sfgate.com",
+	"title": "Recruiter",
+	"city": "Kingersheim"
+}, {
+	"id": "ce389a05-efda-4608-99b7-69dcba159cb9",
+	"gender": "Male",
+	"firstName": "Antonio",
+	"lastName": "Wood",
+	"email": "awooda@flickr.com",
+	"title": "Clinical Specialist",
+	"city": "Santo Ângelo"
+}, {
+	"id": "fc9150a4-01aa-40f0-8fd8-20e83270a009",
+	"gender": "Male",
+	"firstName": "Jeffrey",
+	"lastName": "Rice",
+	"email": "jriceb@mlb.com",
+	"title": "VP Accounting",
+	"city": "Soraya"
+}, {
+	"id": "afe5f7da-2bc9-485f-b052-82fe7d0fc1d1",
+	"gender": "Female",
+	"firstName": "Melissa",
+	"lastName": "Williamson",
+	"email": "mwilliamsonc@t.co",
+	"title": "VP Quality Control",
+	"city": "Vodstroy"
+}, {
+	"id": "52743ef0-0663-4e81-9288-a5b37f04f220",
+	"gender": "Male",
+	"firstName": "Scott",
+	"lastName": "Reynolds",
+	"email": "sreynoldsd@hubpages.com",
+	"title": "Accountant IV",
+	"city": "Lesogorsk"
+}, {
+	"id": "da88c8ec-a431-4d5a-82a5-fbe9eae699aa",
+	"gender": "Male",
+	"firstName": "Phillip",
+	"lastName": "Little",
+	"email": "plittlee@shinystat.com",
+	"title": "Internal Auditor",
+	"city": "Yushan"
+}, {
+	"id": "487daeb6-37de-4c53-93ed-1247807da2c6",
+	"gender": "Female",
+	"firstName": "Angela",
+	"lastName": "Hayes",
+	"email": "ahayesf@yolasite.com",
+	"title": "General Manager",
+	"city": "Aksu"
+}, {
+	"id": "3e93643c-32f2-402f-8eaf-48cfc96e6857",
+	"gender": "Male",
+	"firstName": "Lawrence",
+	"lastName": "Stephens",
+	"email": "lstephensg@apple.com",
+	"title": "Director of Sales",
+	"city": "Altuf’yevskiy"
+}, {
+	"id": "404e30fe-2b9c-46b4-bc9a-8dd42794130e",
+	"gender": "Male",
+	"firstName": "Jeffrey",
+	"lastName": "Ramos",
+	"email": "jramosh@abc.net.au",
+	"title": "Account Executive",
+	"city": "Zásmuky"
+}, {
+	"id": "e2314f8c-8333-4754-96ed-0a8fb0a92d64",
+	"gender": "Male",
+	"firstName": "Donald",
+	"lastName": "Ferguson",
+	"email": "dfergusoni@ucoz.com",
+	"title": "Marketing Manager",
+	"city": "Heemstede"
+}, {
+	"id": "ad9e0cfe-c6d8-4f1b-a8e4-dc0ea7c2fd18",
+	"gender": "Male",
+	"firstName": "Willie",
+	"lastName": "Duncan",
+	"email": "wduncanj@elegantthemes.com",
+	"title": "Registered Nurse",
+	"city": "Kamenný Přívoz"
+}]
